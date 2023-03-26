1d223343fc9562be1bb1a04b2b14ab5e5255a4c2
AgustinEstrada <aguss_979@hotmail.com>
2023-03-26 20:03:47 -0300

    home

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./styles/header.css";
+const Header = () => {
+  return (
+    <div className="header">
+
+      <div className="header-content content-left">
+        <div id="img-left"></div>
+      </div>
+
+      <div className="content-mid">
+        <div id="img-mid"></div>
+        <select name="frutas" className="select">
+          <option value="manzana">Villa Allende</option>
+          <option value="banana">Valle escondido</option>
+          <option value="naranja">Cerro de las rosas</option>
+          <option value="pera">Nueva Cordoba</option>
+        </select>
+      </div>
+
+      <div className="header-content content-right">
+        <div id="img-right"></div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Header;
--- src/home/ViewHome.jsx
+++ src/home/ViewHome.jsx
@@ -3,16 +3,28 @@ import "./styles/homeStyle.css";
 import NavBar from "../components/navbar/NavBar";
 import SocialPromotion from "../components/social-promotion/SocialPromotion";
 import Carrousel from "../components/carousel/Carousel";
-import { slides } from '../components/carousel/assets/exporters';
+import Header from "../components/header/Header";
+import { slides, slides2 } from "../components/carousel/assets/exporters";
 import titleCarousel from "../components/carousel/assets/titulo-efollets-premium.png";
-import { slides2 } from '../components/carousel/assets/exporters';
-import titleCarousel2 from '../components/carousel/assets/titulo-nuevos-beneficios.png'
+import titleCarousel2 from "../components/carousel/assets/titulo-nuevos-beneficios.png";
 const ViewHome = () => {
   return (
     <div className="content-home">
+        <Header />
       <div className="sub-background">
-        <Carrousel slides={slides2} titleCarousel={titleCarousel2} height={'100px'} width={'100px'} heightMid={'115px'} hasText={true}/>
-        <Carrousel slides={slides} titleCarousel={titleCarousel} heightMid={'190px'}/>
+        <Carrousel
+          slides={slides2}
+          titleCarousel={titleCarousel2}
+          height={"100px"}
+          width={"100px"}
+          heightMid={"115px"}
+          hasText={true}
+        />
+        <Carrousel
+          slides={slides}
+          titleCarousel={titleCarousel}
+          heightMid={"190px"}
+        />
         <SocialPromotion />
         <NavBar />
       </div>
